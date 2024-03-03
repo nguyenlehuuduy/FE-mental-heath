165bdb62900d7887b279ca0d25e2f7115589dc37
Duong <duong.nh@open-dev.io>
2024-03-03 13:34:56 +0700

    add open-api-code-gen

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,16 @@
-import MyTextField from "../../../component/Input";
+"use client"
 
-export default function Home() {
+import appClient from "@/util/ApiService";
+
+
+export default async function Home() {
+  // dưới đây là lệnh call api
+  const listImage  = await appClient.images.getImages()
   return (
     <div>
-      <MyTextField name="hai duong yeu vu" />
+      {listImage.data?.map(item => {
+        return(<p>lấy từ db:  {item.decription}</p>)
+      })}
     </div>
   );
 }
