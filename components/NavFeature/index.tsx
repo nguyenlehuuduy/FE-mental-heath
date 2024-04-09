40c5aff3af2de0272ce99b44b560b95735b01256
ZuKerNo1 <trankimvu2002@gmail.com>
2024-04-09 22:03:25 +0700

    Implement UI nav feature

---
+++ components/NavFeature/index.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import React from "react";
+
+const listFeature = [
+  {
+    id: 1,
+    image: "/nav_feature.png",
+    name: "Gửi phiền muộn",
+  },
+  {
+    id: 2,
+    image: "/nav_feature.png",
+    name: "Sound Chill",
+  },
+  {
+    id: 3,
+    image: "/nav_feature.png",
+    name: "Thiền",
+  },
+  {
+    id: 4,
+    image: "/nav_feature.png",
+    name: "2 phút",
+  },
+];
+
+const NavFeature = () => {
+  return (
+    <div className="w-full px-7 py-5">
+      <div className="flex flex-col gap-6">
+        {listFeature.map((item: any) => (
+          <div key={item.id} className="flex flex-row items-center gap-7">
+            <Image
+              src={item.image}
+              width={60}
+              height={60}
+              alt="logo"
+              className="rounded-[10px]"
+            />
+            <p className="text-base font-bold text-[#000000B3]">{item.name}</p>
+          </div>
+        ))}
+        <p className="text-base font-bold text-[#00000099] text-center cursor-pointer">
+          Xem thêm
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NavFeature;
--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Footer, Header, SideBar } from "../../../components";
+import { Footer, Header, NavFeature, SideBar } from "../../../components";
 
 export const metadata: Metadata = {
   title: "metal-heath",
@@ -16,11 +16,17 @@ export default function UserLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className="relative bg-white w-full h-full mx-auto bg-[url('/waves.png')] bg-cover">
+      <body className="relative bg-[#F5F6F8]">
         <Header />
-        {children}
-        <Footer />
-        <SideBar />
+        <div className="max-w-[1440px] pt-[75px] overflow-hidden mx-auto flex">
+          <div className="max-w-[300px] bg-blue-50">
+            <SideBar />
+          </div>
+          <div className="max-w-[720px] px-[300px] mx-auto">{children}</div>
+          <div className="max-w-[300px] bg-white">
+            <NavFeature />
+          </div>
+        </div>
       </body>
     </html>
   );
